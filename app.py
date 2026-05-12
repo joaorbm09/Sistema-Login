5e6d8e4c3dcb0cde522ea06d4fdd2dbb8c0379a8
joaorbm09 <joao.rbm09@gmail.com>
2026-05-12 19:51:05 -0300

    sitema-de-login-refeito:)

--- app.py
+++ app.py
@@ -1,28 +1,21 @@
-from flask import Flask,render_template,request #utilizando mais ferramentas
+from flask import Flask,render_template,request
 
 app = Flask (__name__)
 
-@app.route('/')#rota inicial
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/login',methods=['POST'])#segunda rota, que ira retorna apenas acesso negado e liberado
+@app.route('/login', methods=['POST'])
 def login():
+    
     user = request.form.get('user')
-    passwprd = request.form.get('password')
-
-    if user == "m4tos." and passwprd == "1234":
-        return "Acesso liberado!👌"
-    else: 
-        return "Acesso negado!!😐😶‍🌫️"
+    password = request.form.get('password')
+    
+    if user == "matos" and password == "1234":
+        return "Acesso liberado👍"
+    else:
+        return "Acesso negado❌"
     
 if __name__ == '__main__':
     app.run("0.0.0.0", port=8000)
-
-
-
-
-
-
-
-
